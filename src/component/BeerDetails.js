06b5a1225b110e17504bc992342d49735d428d80
Sarah Damag <sarahdamag@gmail.com>
2020-11-07 16:28:07 +0100

    iteration 4

--- src/component/BeerDetails.js
+++ src/component/BeerDetails.js
@@ -1,16 +1,44 @@
-import React from 'react'
-import Header from './Header'
-
+import React from 'react';
+import Header from './Header';
+import axios from 'axios';
 
 class BeerDetails extends React.Component {
-    render () {
-        return (
-            <div>
-                <Header/>
-                BeerDetails
-            </div>
-        )
-    }
+  state = { beer: {} };
+
+  //récupérer l'id dans le params de l'url
+  findBeer = () => {
+    let id = this.props.match.params.id;
+    console.log(id);
+    //find la bière dans l'API qui correspond à cet id
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/${id}`)
+      .then((beerFromAPI) => {
+        console.log(beerFromAPI);
+        //mettre la bière dans le state
+        this.setState({ beer: beerFromAPI.data });
+      })
+      .catch((err) => console.log("échec dans le fetch d'id", err));
+  };
+
+  componentDidMount() {
+    this.findBeer();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        {/* afficher les clés de la bière dans le rendu BeerDetails */}
+        <img src={this.state.beer.image_url} alt=""></img>
+        <h2>{this.state.beer.name}</h2>
+        <h2>{this.state.beer.tagline}</h2>
+        <p>{this.state.beer.first_brewed}</p>
+        <p>{this.state.beer.attenuation_level}</p>
+        <p>{this.state.beer.description}</p>
+        <p>Created by : {this.state.beer.contributed_by}</p>
+      </div>
+    );
+  }
 }
 
-export default BeerDetails
+export default BeerDetails;
--- src/component/BeersList.js
+++ src/component/BeersList.js
@@ -1,44 +1,45 @@
-import React from 'react'
-import axios from 'axios'
-import Header from './Header'
-
+import React from 'react';
+import axios from 'axios';
+import Header from './Header';
+import { Link } from 'react-router-dom';
 
 class BeersList extends React.Component {
-    state = {
-        listOfBeers: []
-    }
-    getAllBeers = () =>{
-      axios.get(`https://ih-beers-api2.herokuapp.com/beers`)
-      .then(beersFromApi => {
-        console.log('beersfromAPI', beersFromApi)
+  state = {
+    listOfBeers: [],
+  };
+  getAllBeers = () => {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers`)
+      .then((beersFromApi) => {
         this.setState({
-            listOfBeers: beersFromApi.data
-        })
-      })
-    }
+          listOfBeers: beersFromApi.data,
+        });
+      });
+  };
 
-    componentDidMount() {
-        this.getAllBeers();
-      }
-    render () {
-        return (
-            <div>
-                <Header/>
-                <ul>
-                    {this.state.listOfBeers.map(beer => (
-                        <li key={beer._id}>
-                            <img  style={{height:"200px"}}src= {beer.image_url} alt = '' />
-                            <h2>{beer.name}</h2>
-                            <h3>Created by: {beer.contributed_by}</h3>
-                            <h3>{beer.tagline}</h3>
-                        </li>
-                        
-                    ))}
-                </ul>
-                
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.getAllBeers();
+  }
+  
+  render() {
+    return (
+      <div>
+        <Header />
+        <ul>
+          {this.state.listOfBeers.map((beer) => (
+            <li key={beer._id}>
+              <Link to={`/beers/${beer._id}`}>
+                <img style={{ height: '200px' }} src={beer.image_url} alt="" />
+              </Link>
+              <h2>{beer.name}</h2>
+              <h3>Created by: {beer.contributed_by}</h3>
+              <h3>{beer.tagline}</h3>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default BeersList
+export default BeersList;
